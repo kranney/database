b2b349726d1c1c44c72ba9625fb31be7092353dc
Kyle Ranney <kranney@olivetcollege.edu>
2015-03-19 10:23:02 -0400

    redid

--- homework_06.sql
+++ homework_06.sql
@@ -8,12 +8,19 @@ CREATE TABLE hotel
 CREATE TABLE room
 (
 	roomNo, INTEGER NOT NULL PRIMARY KEY,
-	hotelNo INTEGER NOT NULL PRIMARY KEY,
-	type CHAR(10) NOT NULL FOREIGN KEY,
+	hotelNo INTEGER NOT NULL PRIMARY KEY, -- can i have two primary keys?
+	type CHAR(10) NOT NULL FOREIGN KEY, -- foreign key?
 	price MONEY NOT NULL FOREIGN KEY,
-	CHECK (type = 'Single' OR type = 'Double' OR type = 'Family'),
-	CHECK (price >= MONEY(10) AND price <= MONEY(100)),
-	CHECK (roomNo >= 1 AND roomNo <= 100),
+	CHECK (type = 'Single' OR type = 'Double' OR type = 'Family'), -- this is makeing sure that the type of room must be single, double, or family
+	CHECK (price >= MONEY(10) AND price <= MONEY(100)), -- making sure price is between 10 & 100
+	CHECK (roomNo >= 1 AND roomNo <= 100), -- making sure roomNo are between 100 and 1 inclusive
 );
 
-CREATE TABLE
+CREATE TABLE booking
+(
+	hotelNo INTEGER NOT NULL PRIMARY KEY,
+	guestNo INTEGER NOT NULL PRIMARY KEY,
+	dateFrom DATE NOT NULL PRIMARY KEY,
+	dateTo DATE NOT NULL FOREIGN KEY,
+	roomNo INTEGER NOT NULL FOREIGN KEY,
+	)
