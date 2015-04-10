f5ee9e2a41786a4ff279b6ace9e3b2a393b821b5
Kyle Ranney <kranney@olivetcollege.edu>
2015-04-09 20:46:35 -0400

    made syntax changes to database

--- project_table.sql
+++ project_table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE courses
 (
 	time char(16) NOT NULL,
-	number int NOT NULL PRIMARY KEY,
-	name varchar(100) NOT NULL PRIMARY KEY,
+	number int NOT NULL,
+	name varchar(100) NOT NULL,
 	counts_to_majors varchar(200) NOT NULL,
 	number_of_students int NOT NULL,
-	CHECK (number < 0)
-	CHECK (number_of_students > 0)
+	PRIMARY KEY (number, name),
+	CHECK (number < 0 AND number_of_students > 0)
+	
 );
 
 CREATE TABLE professors
@@ -15,18 +16,18 @@ CREATE TABLE professors
 	pay money NOT NULL,
 	full_name varchar(250) NOT NULL,
 	number int NOT NULL,
-	department
+	department varchar(250) NOT NULL,
 	PRIMARY KEY (full_name, number)
 );
 
 CREATE TABLE department
 (
 	director varchar, -- will have a name from professors
-	majors varchar(50) NOT NULL PRIMARY KEY,
-	name varchar(50) NOT NULL PRIMARY KEY,
-	minors varchar(50) NOT NULL PRIMARY KEY,
-	professors, -- foreign key and primary key NOT NULL
-	PRIMARY KEY (majors, name, minors)
+	majors varchar(50) NOT NULL,
+	name varchar(50) NOT NULL,
+	minors varchar(50) NOT NULL,
+	professors varchar(250) NOT NULL,-- foreign key and primary key NOT NULL
+	PRIMARY KEY (majors, name, minors),
 	FOREIGN KEY (professors)
 		REFERENCES professors (full_name)
 );
@@ -34,17 +35,16 @@ CREATE TABLE department
 CREATE TABLE students
 (
 	major01 varchar(50) NOT NULL, --foreign key
-	major02 varchar(50)
-	gpa int char(4) NOT NULL,
-	number int NOT NULL PRIMARY KEY,
+	major02 varchar(50),
+	gpa int NOT NULL,
+	number int NOT NULL,
 	minor01 varchar(50), --foreign key could be null
 	minor02 varchar(50), --foregin key could be null
 	graduation_year int NOT NULL,
 	advisor varchar(250) NOT NULL, --foreign key
-	PRIMARY KEY (number)
+	PRIMARY KEY (number),
 	FOREIGN KEY (major01, major02, minor01, minor02)
-		REFERENCES courses (major01, major02, minor01)
+		REFERENCES courses (major01, major02, minor01),
 	FOREIGN KEY (advisor)
 		REFERENCES professors (full_name)
 );
-
