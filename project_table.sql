6e68ff851aa7dc1ee9a6c35712be98ab84c2a71a
Kyle Ranney <kranney@olivetcollege.edu>
2015-04-12 16:44:00 -0400

    added majors and minors table. working on 3NF

--- project_table.sql
+++ project_table.sql
@@ -1,11 +1,13 @@
 CREATE TABLE courses
 (
 	time char(16) NOT NULL,
-	number int NOT NULL,
+	id int NOT NULL,
 	name varchar(100) NOT NULL,
-	counts_to_majors varchar(200) NOT NULL,
 	number_of_students int NOT NULL,
+	professor_id int NOT NULL,
 	PRIMARY KEY (number, name),
+	FOREIGN KEY (professor_id)
+		REFERENCES professors (id)
 	CHECK (number < 0 AND number_of_students > 0)
 	
 );
@@ -16,21 +18,26 @@ CREATE TABLE professors
 	pay money NOT NULL,
 	last_name varchar(250) NOT NULL,
 	first_name varchar(250) NOT NULL,
-	number int NOT NULL,
-	department varchar(250) NOT NULL,
-	PRIMARY KEY (full_name, number)
+	id int NOT NULL,
+	department_id varchar(250) NOT NULL,
+	PRIMARY KEY (full_name, number),
+	FOREIGN KEY (department_id)
+		REFERENCES departments (id)
 );
 
-CREATE TABLE department
+CREATE TABLE departments
 (
-	director varchar, -- will have a name from professors
+	id int NOT NULL,
+	director_id int, -- will have a name from professors
 	majors varchar(50) NOT NULL,
 	name varchar(50) NOT NULL,
 	minors varchar(50) NOT NULL,
-	professors varchar(250) NOT NULL,-- foreign key and primary key NOT NULL
+	professor_id int NOT NULL,
 	PRIMARY KEY (majors, name, minors),
-	FOREIGN KEY (professors)
-		REFERENCES professors (full_name)
+	FOREIGN KEY (professor_id)
+		REFERENCES professors (id)
+	FOREIGN KEY (director_id)
+		REFERENCES professors (id)
 );
 
 CREATE TABLE students
@@ -41,14 +48,42 @@ CREATE TABLE students
 	major01 varchar(50) NOT NULL, --foreign key
 	major02 varchar(50),
 	gpa number NOT NULL,
-	number int NOT NULL,
+	id int NOT NULL,
 	minor01 varchar(50), --foreign key could be null
 	minor02 varchar(50), --foregin key could be null
 	graduation_year int NOT NULL,
-	advisor varchar(250) NOT NULL, --foreign key
+	advisor_number int NOT NULL,
 	PRIMARY KEY (number),
-	FOREIGN KEY (major01, major02, minor01, minor02)
-		REFERENCES courses (major01, major02, minor01),
-	FOREIGN KEY (advisor)
-		REFERENCES professors (last_name, first_name)
+	FOREIGN KEY (advisor_number)
+		REFERENCES professors (number)
+);
+
+CREATE TABLE professors_courses
+(
+	professor_id int NOT NULL,
+	course_id int NOT NULL,
+	FOREIGN KEY (professor_id)
+		REFERENCES professors (id)
+	FOREIGN KEY (course_id)
+		REFERENCES courses (id)
+);
+
+CREATE TABLE majors
+(
+	name varchar(250) NOT NULL,
+	id int NOT NULL,
+	department_id int NOT NULL,
+	PRIMARY KEY (name, id, department_id),
+	FOREIGN KEY (department_id)
+		REFERENCES departments (id)
+);
+
+CREATE TABLE minors
+(
+	name varchar(250) NOT NULL,
+	id int NOT NULL,
+	department_id int NOT NULL,
+	PRIMARY KEY (name, id),
+	FOREIGN KEY (department_id)
+		REFERENCES departments (id)
 );
