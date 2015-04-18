99925a0b4c7b3e6d8b1f733a401cf11c17690d52
Kyle Ranney <kranney@olivetcollege.edu>
2015-04-17 22:53:18 -0400

    adding methods

--- project_object.py
+++ project_object.py
@@ -5,4 +5,31 @@ cur = con.cursor()
 class College:
 	""" Information about students, professors, courses, and departments"""
 
-	def __init__(self, cur)
+	def __init__(self, connection):
+		self.connection = connection
+
+	def add_student(self, info):
+		"""Will allow the user to add a student to the students table."""
+
+		with self.connection.cursor() as cur:
+			cur.execute("""INSERT INTO students VALUES""", info)
+
+	def add_professor(self, info):
+		""" Will allow the user to add a professor to the professors table."""
+
+		with self.connection.cursor() as cur:
+			cur.execute("""INSERT INTO professors VALUES""", info)
+
+	def add_course(self, info):
+		""" Will allow the user to add a course to the courses table."""
+
+		with self.connection.cursor() as cur:
+			cur.execute("""INSERT INTO courses VALUES""", info)
+
+	def add_department(self, info):
+		""" Will allow the user to add a department to the departments table."""
+
+		with self.connection.cursor() as cur:
+			cur.execute("""INSERT INTO courses VALUES""", info)
+
+	def view_
