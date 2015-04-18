4cf3e6b7ae9da0f50926ed3e4ea319af1ba76832
Kyle Ranney <kranney@olivetcollege.edu>
2015-04-17 21:41:16 -0400

    creating object oriented program for project

---
+++ project_object.py
@@ -0,0 +1,8 @@
+class College:
+	""" Information about students, professors, courses, and departments"""
+
+	def __init__(self, first_name, middle_name, last_name, major01, major02, 
+		minor01, minor02, gpa, id, graduation_year, advisor_id, course_id01,
+		 course_id02, course_id03, course_id04, course_id05, course_id06, 
+		 course_id07, course_id08):
+	(student, )
