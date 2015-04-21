243d5430246120dffa0f0f9ec8818818776bc201
Kyle Ranney <kranney@olivetcollege.edu>
2015-04-20 22:51:28 -0400

    fixed lines 199 and 207

--- project_object.py
+++ project_object.py
@@ -18,7 +18,7 @@ class College:
 			cur.execute("""INSERT INTO students VALUES 
 			(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """, info)
 
-		commit()
+		self.commit()
 
 	def add_professor(self, info):
 		""" Will allow the user to add a professor to the professors table."""
@@ -26,7 +26,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""INSERT INTO professors VALUES (%s, %s, %s, %s, %s, %s)""", info)
 
-		commit()
+		self.commit()
 
 	def add_course(self, info):
 		""" Will allow the user to add a course to the courses table."""
@@ -34,7 +34,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""INSERT INTO courses VALUES (%s, %s, %s, %s)""", info)
 
-		commit()
+		self.commit()
 
 	def add_department(self, info):
 		""" Will allow the user to add a department to the departments table."""
@@ -42,7 +42,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""INSERT INTO courses VALUES (%s, %s, %s)""", info)
 
-		commit()
+		self.commit()
 
 	def add_major(self, info):
 		""" Will allow the user to add a major to the majors table."""
@@ -50,7 +50,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""INSERT INTO majors VALUES (%s, %s, %s)""", info)
 
-		commit()
+		self.commit()
 
 	def add_minor(self, info):
 		""" Will allow the user to add a minor to the minors table."""
@@ -58,7 +58,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""INSERT INTO minors VALUES (%s, %s, %s)""", info)
 
-		commit()
+		self.commit()
 
 	def view_student(self, student_id):
 		""" Will allow the user to view a student from the table."""
@@ -66,7 +66,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM students WHERE name = (%s)""", student_id)
 
-		commit()
+		self.commit()
 
 	def view_professor(self, professor_id):
 		""" Will allow the user to view a professor from the table."""
@@ -74,7 +74,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM students WHERE id = (%s)""", professor_id)
 
-		commit()
+		self.commit()
 
 	def view_course(self, course_id):
 		""" Will allow the user to view a course from the courses table."""
@@ -82,7 +82,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM students WHERE id = (%s)""", course_id)
 
-		commit()
+		self.commit()
 
 	def view_department(self, department_id):
 		""" Will allow the user to viwe a department from the table."""
@@ -90,7 +90,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM departments WHERE id = (%s)""", department_id)
 
-		commit()
+		self.commit()
 
 	def view_major(self, major_id):
 		""" Will allow the user to view a major from the majors table."""
@@ -98,7 +98,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM majors WHERE id = (%s)""", major_id)
 
-		commit()
+		self.commit()
 
 	def view_minor(self, minor_id):
 		""" Will allow the user to view a minor from the minors table."""
@@ -106,7 +106,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""SELECT * FROM minors WHERE id = (%s)""", minor_id)
 
-		commit()
+		self.commit()
 
 	def delete_student(self, student_id):
 		""" Will allow the user to delete a student from the students table."""
@@ -114,7 +114,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM students WHERE id = (%s)""", student_id)
 
-		commit()
+		self.commit()
 
 	def delete_professor(self, professor_id):
 		""" Will allow the user to delete a professor from the professors table."""
@@ -122,7 +122,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM professors WHERE id = (%s)""", professor_id)
 
-		commit()
+		self.commit()
 
 	def delete_course(self, course_id):
 		""" Will allow the user to delete a course from the courses table."""
@@ -130,7 +130,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM courses WHERE id = (%s)""", course_id)
 
-		commit()
+		self.commit()
 
 	def delete_department(self, department_id):
 		""" Will allow the user to delete a department from the table."""
@@ -138,7 +138,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM departments WHERE id = (%s)""", department_id)
 
-		commit()
+		self.commit()
 
 	def delete_major(self, major_id):
 		""" Will allow the user to delete a major from the majors table."""
@@ -146,7 +146,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM majors WHERE id = (%s)""", major_id)
 
-		commit()
+		self.commit()
 
 	def delete_minor(self, minor_id):
 		""" Will allow the user to delete a minor from the minors table."""
@@ -154,7 +154,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""DELETE * FROM minors WHERE id = %s""", minor_id)
 
-		commit()
+		self.commit()
 
 	def update_student(self, info):
 		""" Will allow the user to enter the studnet's id and update that
@@ -163,7 +163,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE students SET column = (%s) WHERE id = (%s)""", info)
 
-		commit()
+		self.commit()
 
 	def update_professor(self, info):
 		""" Will allow the user to enter the professor's id and update that
@@ -172,7 +172,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE professors SET column = (%s) WHERE id = (%s)""", info)
 
-		commit()
+		self.commit()
 
 	def update_course(self, info):
 		""" Will allow the user to enter a course id and update that particular course."""
@@ -180,7 +180,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE courses SET column = (%s) WHERE id = (%s)""", info)
 
-		commit()
+		self.commit()
 
 	def update_department(self, info):
 		""" Will allow the user to enter a department id and update that particular department."""
@@ -188,7 +188,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE departments SET column = (%s) WHERE id = (%s)""", info)
 
-		commit()
+		self.commit()
 
 	def update_major(self, info):
 		""" Will allow the user to enter a major id and update a particular major."""
@@ -196,7 +196,7 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE majors SET column = (%s) WHERE id = (%s)""", info)
 
-		commit()
+		self.commit()
 
 	def update_minor(self, info):
 		""" Will allow the user to enter a minor id and update a particular minor."""
@@ -204,4 +204,4 @@ class College:
 		with self.connection.cursor() as cur:
 			cur.execute("""UPDATE minors SET column = (%s) WHERE id = (%s)""", info)
 
-		self.connection.commit()
+		self.commit()
